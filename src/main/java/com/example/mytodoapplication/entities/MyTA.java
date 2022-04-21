3da4d7b38841f87730c346341771ef41755f18b7
lasiru18 <lstechwork@gmai9l.com>
2022-04-22 01:31:31 +0530

    initial commit

---
+++ src/main/java/com/example/mytodoapplication/entities/MyTA.java
@@ -0,0 +1,29 @@
+package com.example.mytodoapplication.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "MyTA")
+public class MyTA {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "completed")
+    private Boolean completed;
+
+}
+
